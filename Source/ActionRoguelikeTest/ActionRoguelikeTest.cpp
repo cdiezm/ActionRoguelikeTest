f45cbc55682c789e3f8504faaac1203eef9b2346
cdiezm <cdiezm14@gmail.com>
2025-05-19 16:19:00 +0200

    First Commit - Project creation

---
+++ Source/ActionRoguelikeTest/ActionRoguelikeTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionRoguelikeTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRoguelikeTest, "ActionRoguelikeTest" );
